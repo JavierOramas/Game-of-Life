7cbfe3b7e418ce13cfbaddccba50da17519ea4ad
Javier Oramas <javiale2000@gmail.com>
2020-09-18 23:23:53 -0400

    creating grid

--- main.py
+++ main.py
@@ -2,3 +2,32 @@ import pygame
 import numpy as np
 
 pygame.init()
+
+width, heigth = 1000,1000
+screen = pygame.display.set_mode((height, width))
+
+bg = 25,25,25
+screen.fill(bg)
+
+nxC, nyC = 25,25
+
+dimCW = width / nxC
+dimCH = heigth / nyC
+
+
+
+while True:
+    
+    for y in range(0, nxC):
+        for x in range(nyC):
+            
+            poly = [
+                ((x)   * dimCW ,   y * dimCH),
+                ((x+1) * dimCW ,   y * dimCH),
+                ((x+1) * dimCW , ( y + 1 ) * dimCH),
+                ((x)   * dimCW , ( y + 1 ) * dimCH),
+            ]
+            
+            pygame.draw.polygon(screen, (128,128,128), poly, width=1)
+
+    pygame.display.flip()
